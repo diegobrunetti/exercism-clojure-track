d05893134b8aef5abecc702a3bc4e60a73cf8b1e
Diego Brunetti Teixeira <diegoonsoftware@gmail.com>
2025-03-06 23:26:34 -0300

    Refactoring.
    Refactoring.

--- armstrong-numbers/src/armstrong_numbers.clj
+++ armstrong-numbers/src/armstrong_numbers.clj
@@ -16,18 +16,3 @@
          (map #(.pow (biginteger %) exponent))
          (reduce +)
          (== num))))
-
-
-;; (ns armstrong-numbers)
-
-;; (defn- split [n]
-;;   (loop [result '()
-;;          m n]
-;;     (if (< m 10)
-;;       (cons m result)
-;;       (recur (cons (mod m 10) result) (quot m 10)))))
-
-;; (defn armstrong? [num]
-;;   (let [digits (split num)
-;;         digit-count (count digits)]
-;;     (== num (reduce (fn [sum digit] (+ sum (.pow (biginteger digit) digit-count))) 0 digits))))
