47c25f3d96057bded3b74c02338d848928cd9854
Diego Brunetti Teixeira <diegoonsoftware@gmail.com>
2025-04-02 21:36:49 -0300

    Binary Search
    Added a solution to the "Binary Search" exercise.

---
+++ binary-search/src/binary_search.clj
@@ -0,0 +1,14 @@
+(ns binary-search)
+
+(defn search-for
+  "Returns the index of num in coll, or -1 if num is not found."
+  [num coll]
+  (loop [start 0
+         end (- (count coll) 1)]
+    (let [index (quot (+ start end) 2)
+          mid (get coll index)]
+      (cond
+        (> start end) -1
+        (= mid num) index
+        (> mid num) (recur 0 (dec index))
+        (< mid num) (recur (inc index) end)))))
