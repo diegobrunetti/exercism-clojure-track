58ee574724b676aa9282e42e5fa40d1213430ab2
Diego Brunetti Teixeira <diegoonsoftware@gmail.com>
2025-04-05 01:32:47 -0300

    Largest Series
    Refactoring.

--- largest-series-product/src/largest_series_product.clj
+++ largest-series-product/src/largest_series_product.clj
@@ -1,25 +1,24 @@
 (ns largest-series-product)
 
-(defn validate [span input]
+(defn- fail [message]
+  (throw (IllegalArgumentException. message)))
+
+(defn- validate [span input]
   (cond
-    (some? (re-find #"\D" input)) "digits input must only contain digits"
-    (> span (count input)) "span must not exceed string length"
-    (neg? span) "span must not be negative"))
+    (some? (re-find #"\D" input)) (fail "digits input must only contain digits")
+    (> span (count input)) (fail "span must not exceed string length")
+    (neg? span) (fail "span must not be negative")
+    :else input))
 
-(defn get-digits [input]
-  (map #(Character/digit % 10) input))
-
-(defn get-series [span, input] (partition span 1 [0] input))
-(defn get-product [series] (map (partial apply *) series))
-(defn get-largest [products] (apply max products))
+(defn- get-digits [input] (map #(Character/digit % 10) input))
+(defn- get-series [span, input] (partition span 1 [0] input))
+(defn- get-product [series] (map (partial apply *) series))
+(defn- get-largest [products] (apply max products))
 
 (defn largest-product [span input]
-  (let [error (validate span input)]
-    (when (some? error)
-      (throw (IllegalArgumentException. error)))
-
-    (->> (get-digits input)
-         (get-series span)
-         (get-product)
-         (get-largest))))
+  (->> (validate span input)
+       (get-digits)
+       (get-series span)
+       (get-product)
+       (get-largest)))
  
