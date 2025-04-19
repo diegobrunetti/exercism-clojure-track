68fd2102f991570a2a8854afb3a82884957b52b0
Diego Brunetti Teixeira <diegoonsoftware@gmail.com>
2025-04-19 17:04:42 -0300

    Luhn
    Added a solution to the "Luhn" exercise.

---
+++ luhn/src/luhn.clj
@@ -0,0 +1,20 @@
+(ns luhn
+  (:require [clojure.string :as str]))
+
+(defn- luhn-double [n]
+  (if (> n 4) (- (+ n n) 9) (+ n n)))
+
+(defn- zigzag-collect [coll]
+  (flatten (partition 1 2 coll)))
+
+(defn valid?
+  "Returns true if the given number is valid according
+  to the luhn algorithm; otherwise, returns false."
+  [s]
+  (let [s (reverse (str/replace s #"\s" ""))]
+    (and (< 1 (count s))
+         (let [parsed      (map #(Character/digit % 10) s)
+               transformed (map luhn-double (zigzag-collect (drop 1 parsed)))
+               untouched   (flatten (zigzag-collect parsed))
+               sum         (reduce + (concat transformed untouched))]
+           (zero? (rem sum 10))))))
